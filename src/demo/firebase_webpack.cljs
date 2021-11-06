86e989c7ea42511c88bc6c78f6f0af77f472ab4b
athomasoriginal <thomasmattacchione@gmail.com>
2021-11-06 10:01:40 -0400

    Use firebase: Method #1
    This approach is failing at the `getAuth` with an error message in the
    console of:
    
    - `Error: Component auth has not been registered yet`

--- src/demo/firebase_webpack.cljs
+++ src/demo/firebase_webpack.cljs
@@ -18,9 +18,38 @@
   (mount))
 
 
+(goog-define firebase-api-key "AIzaSyB-imAo26zTQr0e3yDefHX32fu-O_sS1Dw")
+
+(goog-define firebase-auth-domain "cljs-firebase-48930.firebaseapp.com")
+
+(goog-define firebase-project-id "cljs-firebase-48930")
+
+(goog-define firebase-storage-bucket "cljs-firebase-48930.appspot.com")
+
+(goog-define firebase-messaging-sender-id "764996571280")
+
+(goog-define firebase-app-id "1:764996571280:web:0677a2707acf7e565cc815")
+
+(goog-define firebase-app-measurement-id "G-30KWZ7XC4X")
+
+(goog-define firebase-use-auth-emulator false)
+
+
+(def firebase-config
+  #js {:apiKey            firebase-api-key
+       :appId             firebase-app-id
+       :authDomain        firebase-auth-domain
+       :messagingSenderId firebase-messaging-sender-id
+       :projectId         firebase-project-id
+       :storageBucket     firebase-storage-bucket})
+
+
 (defonce start-up
   (do
 
-    (mount)
+    (let [app      (js-invoke firebase "initializeApp" firebase-config)
+          app-auth (js-invoke auth "getAuth" app)]
 
-    true))
+      (mount)
+
+      true)))
